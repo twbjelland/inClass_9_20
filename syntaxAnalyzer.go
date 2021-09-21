a07a554865a61d997057e8a9cd0f5820da138a47
twbjelland <60273828+twbjelland@users.noreply.github.com>
2021-09-20 17:23:51 -0700

    define tokens

--- syntaxAnalyzer.go
+++ syntaxAnalyzer.go
@@ -6,10 +6,34 @@ import (
     "os"
 )
 
+//define grammar
+
+//int literal
+//identifier
+//assign_op
+//add_op
+//sub_op
+//mult_op
+//div_op
+//left_paren
+//right_paren
+
 func main() {
+    
+    //read in file
     content, err := os.ReadFile("file.txt")
     if err != nil {
         log.Fatal(err)
     }
     fmt.Println(string(content))
+    
+    //iterate over all characters
+    
+        //find lexemes
+    
+        //convert to tokens
+    
+        //enforce rules
+    
+    
 }
