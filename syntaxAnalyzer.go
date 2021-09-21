20ff431f1785269df3f6ea36d8ef14567b9da11a
twbjelland <60273828+twbjelland@users.noreply.github.com>
2021-09-20 22:07:17 -0700

    add variables

--- syntaxAnalyzer.go
+++ syntaxAnalyzer.go
@@ -5,9 +5,33 @@ import (
     "log"
     "os"
     "unicode"
+    "regexp"
 )
 
+var charClass int
 var next_char rune
+var lexeme string
+var lexLen int
+var token int
+var nextToken int
+var in_fp
+
+var isAlpha = regexp.MustCompile(`^[a-zA-Z]+$`).MatchString
+
+const LETTER = 0
+const DIGIT = 1
+const UNKNOWN = 99
+
+const INT_LIT = 10
+const IDENT = 11
+const ASSIGN_OP = 20
+const ADD_OP = 21
+const SUB_OP = 22
+const MULT_OP = 23
+const DIV_OP = 24
+const LEFT_PAREN = 25
+const RIGHT_PAREN = 26
+
 
 //define grammar
 
@@ -20,6 +44,23 @@ var next_char rune
 //div_op
 //left_paren
 //right_paren
+
+func addChar() {
+    lexeme[lexLen++] = nextChar
+    lexeme[lexLen] = 0
+}
+
+func getChar(){
+    if((nextChar = getc(in_fp)) = EOF) {
+        if (isAlpha(nextChar))
+            charClass = LETTER
+        else if (unicode.isDigit(nextChar))
+            charClass = DIGIT
+            else charClass = UKNOWN;
+    }
+    else
+        charClass = EOF;
+}
 
 func getNonBlank(){
     if unicode.IsSpace(next_char){
