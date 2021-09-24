72480d30c94f2c9fef0dcb089785d020bd4ed111
mtoddwestmont <90293087+mtoddwestmont@users.noreply.github.com>
2021-09-24 14:33:28 -0700

    added comments

--- parser.go
+++ parser.go
@@ -10,6 +10,7 @@ import (
 	"unicode"
 )
 
+//assign each operator a token
 const INT_LIT = 10
 const IDENT = 11
 const ASSIGN_OP = 20
@@ -35,6 +36,7 @@ const DIGIT = 1
 const UNKNOWN = 99
 const EOF = 98
 
+//function to lookup operators and return the token that they were assigned
 func lookup(ch rune) int {
 	switch ch {
 	case '(':
@@ -69,12 +71,14 @@ func lookup(ch rune) int {
 	return nextToken
 }
 
+//a function to add Char to the lexeme
 func addChar() {
 	lexeme[lexLen] = nextChar
 	lexLen++
 	lexeme[lexLen] = 0
 }
 
+//function to get next character of input and determine its character class
 func getChar() {
 	in_fp.Scan()
 	nextString := in_fp.Text()
@@ -93,12 +97,14 @@ func getChar() {
 
 }
 
+//function to call getChar until it returns a non-whitespace character
 func getNonBlank() {
 	for unicode.IsSpace(nextChar) && charClass != EOF {
 		getChar()
 	}
 }
 
+//a lexical analyzer for arithmetic expressions
 func lex() int {
 	lexLen = 0
 	getNonBlank()
